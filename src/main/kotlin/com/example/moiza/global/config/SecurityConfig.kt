42d0c9407c4d1b98a55060144f5d5d82a7d7d5ef
anys34 <anyeseong34@gmail.com>
2024-09-24 00:30:20 +0900

    feat(security): security config 작성

---
+++ src/main/kotlin/com/example/moiza/global/config/SecurityConfig.kt
@@ -0,0 +1,55 @@
+package com.example.moiza.global.config
+
+import com.example.moiza.global.security.jwt.JwtTokenFilter
+import com.example.moiza.global.security.jwt.JwtTokenProvider
+import org.springframework.context.annotation.Bean
+import org.springframework.context.annotation.Configuration
+import org.springframework.http.HttpStatus
+import org.springframework.security.config.Customizer
+import org.springframework.security.config.annotation.web.builders.HttpSecurity
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity
+import org.springframework.security.config.http.SessionCreationPolicy
+import org.springframework.security.web.SecurityFilterChain
+import org.springframework.security.web.authentication.HttpStatusEntryPoint
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter
+
+@EnableWebSecurity
+@Configuration
+class SecurityConfig(
+    private val jwtProvider: JwtTokenProvider
+) {
+    @Bean
+    fun filterChain(http: HttpSecurity): SecurityFilterChain {
+        http
+            .csrf { it.disable() }
+            .httpBasic { it.disable() }
+            .formLogin { it.disable() }
+            .logout { it.disable() }
+
+        http
+            .sessionManagement { session ->
+                session.sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+            }
+
+        http
+            .addFilterBefore (
+                JwtTokenFilter(jwtProvider),
+                UsernamePasswordAuthenticationFilter::class.java
+            )
+
+        http
+            .cors( Customizer.withDefaults() )
+            .authorizeHttpRequests { authorize ->
+                authorize.requestMatchers("/user").authenticated()
+                authorize.anyRequest().permitAll()
+            }
+
+
+        http
+            .exceptionHandling { handler ->
+                handler.authenticationEntryPoint(HttpStatusEntryPoint(HttpStatus.UNAUTHORIZED))
+            }
+
+        return http.build()
+    }
+}
