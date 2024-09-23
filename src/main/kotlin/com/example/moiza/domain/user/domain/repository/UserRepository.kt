468e28e562fa7a0a84c311e8877acecb2e9c2eee
anys34 <anyeseong34@gmail.com>
2024-09-23 17:32:43 +0900

    feat(user): user facade

--- src/main/kotlin/com/example/moiza/domain/user/domain/repository/UserRepository.kt
+++ src/main/kotlin/com/example/moiza/domain/user/domain/repository/UserRepository.kt
@@ -4,4 +4,5 @@ import com.example.moiza.domain.user.domain.User
 import org.springframework.data.repository.CrudRepository
 
 interface UserRepository: CrudRepository<User, Long> {
+    fun findByEmail(email: String): User?
 }
---
+++ src/main/kotlin/com/example/moiza/domain/user/facade/UserFacade.kt
@@ -0,0 +1,19 @@
+package com.example.moiza.domain.user.facade
+
+import com.example.moiza.domain.user.domain.User
+import com.example.moiza.domain.user.domain.repository.UserRepository
+import org.springframework.security.core.context.SecurityContextHolder
+import org.springframework.stereotype.Component
+
+@Component
+class UserFacade(
+    private val userRepository: UserRepository
+) {
+    fun getCurrentUser(): User {
+        val email: String = SecurityContextHolder.getContext().authentication.name
+        return getUserByEmail(email)
+    }
+
+    fun getUserByEmail(email: String)
+            = userRepository.findByEmail(email) ?: throw IllegalArgumentException("Not Found User")
+}
