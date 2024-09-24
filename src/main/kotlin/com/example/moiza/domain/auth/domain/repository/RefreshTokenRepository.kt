16e3d6e8128a8b16467bc1fc8db616380a6490e5
anys34 <anyeseong34@gmail.com>
2024-09-24 12:26:54 +0900

    fat(jpa): repository annotation 추가

--- src/main/kotlin/com/example/moiza/domain/auth/domain/repository/RefreshTokenRepository.kt
+++ src/main/kotlin/com/example/moiza/domain/auth/domain/repository/RefreshTokenRepository.kt
@@ -2,6 +2,8 @@ package com.example.moiza.domain.auth.domain.repository
 
 import com.example.moiza.domain.auth.domain.RefreshToken
 import org.springframework.data.repository.CrudRepository
+import org.springframework.stereotype.Repository
 
+@Repository
 interface RefreshTokenRepository: CrudRepository<RefreshToken, String> {
 }
--- src/main/kotlin/com/example/moiza/domain/user/domain/repository/UserRepository.kt
+++ src/main/kotlin/com/example/moiza/domain/user/domain/repository/UserRepository.kt
@@ -2,7 +2,9 @@ package com.example.moiza.domain.user.domain.repository
 
 import com.example.moiza.domain.user.domain.User
 import org.springframework.data.repository.CrudRepository
+import org.springframework.stereotype.Repository
 
+@Repository
 interface UserRepository: CrudRepository<User, Long> {
     fun findByEmail(email: String): User?
 }
