9dda8a85995a6c04dc0cfdbbc1d2a817fc7f8025
anys34 <anyeseong34@gmail.com>
2024-09-24 12:25:22 +0900

    refactor(refreshToken): 불변 유지

--- src/main/kotlin/com/example/moiza/domain/auth/domain/RefreshToken.kt
+++ src/main/kotlin/com/example/moiza/domain/auth/domain/RefreshToken.kt
@@ -4,7 +4,7 @@ import org.springframework.data.annotation.Id
 import org.springframework.data.redis.core.RedisHash
 
 @RedisHash(value = "refreshToken", timeToLive = 2592000L)
-class RefreshToken(
+data class RefreshToken(
     @Id
     val token: String,
     val email: String
