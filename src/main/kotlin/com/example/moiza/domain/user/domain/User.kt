328d18f33a8f2e14ac76868ede8a65411d082a8f
anys34 <anyeseong34@gmail.com>
2024-09-24 16:29:03 +0900

    feat(user): colunum 추가

--- src/main/kotlin/com/example/moiza/domain/user/domain/User.kt
+++ src/main/kotlin/com/example/moiza/domain/user/domain/User.kt
@@ -1,10 +1,7 @@
 package com.example.moiza.domain.user.domain
 
 import com.example.moiza.domain.user.domain.type.Authority
-import jakarta.persistence.Entity
-import jakarta.persistence.GeneratedValue
-import jakarta.persistence.GenerationType
-import jakarta.persistence.Id
+import jakarta.persistence.*
 
 @Entity
 class User(
@@ -16,15 +13,20 @@ class User(
     @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
     val id: Long = 0L
 
+    @Column(columnDefinition = "VARCHAR(40)", nullable = false, unique = true)
     var email: String = email
         protected set
 
+    @Column(columnDefinition = "VARCHAR(20)", nullable = false)
     var nickname: String = nickname
         protected set
 
+    @Column(nullable = false)
     var profile: String = profile
         protected set
 
+    @Column(nullable = false)
+    @Enumerated(EnumType.STRING)
     var authority: Authority = authority
         protected set
 }
