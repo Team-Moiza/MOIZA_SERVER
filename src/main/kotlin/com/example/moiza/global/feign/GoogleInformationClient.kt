dbad7d707c1faa8f04a5a29e9e8a39790410dac8
anys34 <anyeseong34@gmail.com>
2024-09-24 15:46:10 +0900

    refactor(feign): reqeustParam 사용

--- src/main/kotlin/com/example/moiza/global/feign/GoogleInformationClient.kt
+++ src/main/kotlin/com/example/moiza/global/feign/GoogleInformationClient.kt
@@ -3,10 +3,10 @@ package com.example.moiza.global.feign
 import com.example.moiza.global.feign.auth.dto.res.GoogleInformationResponse
 import org.springframework.cloud.openfeign.FeignClient
 import org.springframework.web.bind.annotation.GetMapping
-import org.springframework.web.bind.annotation.PathVariable
+import org.springframework.web.bind.annotation.RequestParam
 
 @FeignClient(name = "GoogleInformationClient", url = "https://www.googleapis.com/oauth2/v1/userinfo")
 interface GoogleInformationClient {
-    @GetMapping("?alt=json&access_token={TOKEN}")
-    fun getInformation(@PathVariable("TOKEN") accessToken: String): GoogleInformationResponse
+    @GetMapping("?alt=json")
+    fun getInformation(@RequestParam access_token: String): GoogleInformationResponse
 }
