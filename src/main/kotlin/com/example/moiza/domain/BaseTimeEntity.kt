f9e85f6993f5366d9e0b8a68bc9432116ce685ad
김명진 <4mj.eo@dsm.hs.kr>
2024-10-04 15:59:59 +0900

    feat(community): updatedAt 컬럼 추가

--- src/main/kotlin/com/example/moiza/domain/BaseTimeEntity.kt
+++ src/main/kotlin/com/example/moiza/domain/BaseTimeEntity.kt
@@ -2,11 +2,16 @@ package com.example.moiza.domain
 
 import jakarta.persistence.Column
 import jakarta.persistence.MappedSuperclass
+import org.hibernate.annotations.UpdateTimestamp
 import java.time.LocalDateTime
 
 @MappedSuperclass
 abstract class BaseTimeEntity(
 
     @Column(nullable = false, updatable = false, columnDefinition = "DATETIME(6)")
-    val createdAt: LocalDateTime = LocalDateTime.now()
+    val createdAt: LocalDateTime = LocalDateTime.now(),
+
+    @UpdateTimestamp
+    @Column(nullable = false, columnDefinition = "DATETIME(6)")
+    var updatedAt: LocalDateTime = LocalDateTime.now()
 )
