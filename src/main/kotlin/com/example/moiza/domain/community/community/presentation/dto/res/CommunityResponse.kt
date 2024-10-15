7b74eed519cb41889491d0ec4ae0332770bcbae7
김명진 <4mj.eo@dsm.hs.kr>
2024-10-15 23:36:16 +0900

    refactor(community): response 값 수정

--- src/main/kotlin/com/example/moiza/domain/community/community/presentation/dto/res/CommunityResponse.kt
+++ src/main/kotlin/com/example/moiza/domain/community/community/presentation/dto/res/CommunityResponse.kt
@@ -1,9 +1,22 @@
 package com.example.moiza.domain.community.community.presentation.dto.res
 
-import com.example.moiza.domain.community.poll.presentation.dto.res.PollResponse
-import com.example.moiza.domain.community.post.presentation.dto.res.PostResponse
+import com.example.moiza.domain.community.community.domain.entity.type.CommunityType
+import com.example.moiza.domain.community.poll.presentation.dto.res.PollOptionResponse
+import com.example.moiza.domain.user.presentation.dto.res.UserResponse
+import com.fasterxml.jackson.annotation.JsonInclude
+import java.time.LocalDateTime
 
 data class CommunityResponse(
-    val posts: List<PostResponse>,
-    val polls: List<PollResponse>
+    val contents: List<CommunityItemResponse>
+)
+
+@JsonInclude(JsonInclude.Include.NON_NULL)
+data class CommunityItemResponse(
+    val id: Long,
+    val title: String,
+    val content: String,
+    val type: CommunityType,
+    val user: UserResponse,
+    val createdAt: LocalDateTime,
+    val options: List<PollOptionResponse>? = null
 )
