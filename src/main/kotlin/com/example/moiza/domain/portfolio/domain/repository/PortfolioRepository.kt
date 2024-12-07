d8bb024cda149762129d4a868bf8ee2dd1df4a86
김명진 <4mj.eo@dsm.hs.kr>
2024-12-07 14:04:11 +0900

    refactor(portfolio): findByIdAndUser 사용

--- src/main/kotlin/com/example/moiza/domain/portfolio/domain/repository/PortfolioRepository.kt
+++ src/main/kotlin/com/example/moiza/domain/portfolio/domain/repository/PortfolioRepository.kt
@@ -1,10 +1,12 @@
 package com.example.moiza.domain.portfolio.domain.repository
 
 import com.example.moiza.domain.portfolio.domain.Portfolio
+import com.example.moiza.domain.user.domain.User
 import org.springframework.data.jpa.repository.JpaRepository
 import org.springframework.stereotype.Repository
 
 @Repository
 interface PortfolioRepository: JpaRepository<Portfolio, Long> {
     fun findPortfolioByUserId(userId: Long): List<Portfolio>
+    fun findPortfolioByIdAndUser(portfolioId: Long, user: User): Portfolio?
 }
--- src/main/kotlin/com/example/moiza/domain/portfolio/service/DeletePortfolioService.kt
+++ src/main/kotlin/com/example/moiza/domain/portfolio/service/DeletePortfolioService.kt
@@ -2,15 +2,21 @@ package com.example.moiza.domain.portfolio.service
 
 import com.example.moiza.domain.portfolio.domain.repository.PortfolioRepository
 import com.example.moiza.domain.portfolio.exception.PortfolioNotFoundException
+import com.example.moiza.domain.user.domain.User
+import com.example.moiza.domain.user.facade.UserFacade
+import org.hibernate.validator.constraints.Currency
 import org.springframework.data.repository.findByIdOrNull
 import org.springframework.stereotype.Service
 
 @Service
 class DeletePortfolioService(
-    private val portfolioRepository: PortfolioRepository
+    private val portfolioRepository: PortfolioRepository,
+    private val userFacade: UserFacade
 ) {
     fun execute(portfolioId: Long) {
-        val portfolio = portfolioRepository.findByIdOrNull(portfolioId) ?: throw PortfolioNotFoundException
+        val user = userFacade.getCurrentUser()
+        val portfolio = portfolioRepository.findPortfolioByIdAndUser(portfolioId, user)
+            ?: throw PortfolioNotFoundException
 
         portfolioRepository.delete(portfolio)
     }
