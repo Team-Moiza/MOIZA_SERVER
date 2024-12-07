034d951c4638d5a9a431e095611228b9a7f0279e
김명진 <4mj.eo@dsm.hs.kr>
2024-12-07 14:27:36 +0900

    refactor(portfolio): 리스트 조회 로직 변경

--- src/main/kotlin/com/example/moiza/domain/portfolio/service/QueryPortfolioListService.kt
+++ src/main/kotlin/com/example/moiza/domain/portfolio/service/QueryPortfolioListService.kt
@@ -2,19 +2,20 @@ package com.example.moiza.domain.portfolio.service
 
 import com.example.moiza.domain.portfolio.domain.repository.PortfolioRepository
 import com.example.moiza.domain.portfolio.presentation.dto.res.PortfolioListResponse
-import com.example.moiza.domain.user.facade.UserFacade
 import jakarta.transaction.Transactional
 import org.springframework.stereotype.Service
 
 @Service
 class QueryPortfolioListService(
     private val portfolioRepository: PortfolioRepository,
-    private val userFacade: UserFacade
 ) {
     @Transactional
     fun execute(): List<PortfolioListResponse> {
-        val user = userFacade.getCurrentUser()
-        return portfolioRepository.findPortfolioByUserId(user.id).map { portfolio ->
+        val portfolios = portfolioRepository.findAll()
+
+        return portfolios.map { portfolio ->
+            val user = portfolio.user
+
             PortfolioListResponse(
                 id = portfolio.id,
                 name = user.nickname,
