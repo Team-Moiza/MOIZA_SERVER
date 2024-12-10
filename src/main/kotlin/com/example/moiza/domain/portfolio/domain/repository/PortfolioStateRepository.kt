70cdba1299373eee3f291f11ffb32c479af6d349
anys34 <anyeseong34@gmail.com>
2024-12-11 04:44:36 +0900

    feat(portfolioState): portfolioState 삭제

--- src/main/kotlin/com/example/moiza/domain/portfolio/domain/repository/PortfolioStateRepository.kt
+++
@@ -1,9 +0,0 @@
-package com.example.moiza.domain.portfolio.domain.repository
-
-import com.example.moiza.domain.portfolio.domain.PortfolioState
-import org.springframework.data.jpa.repository.JpaRepository
-import org.springframework.stereotype.Repository
-
-@Repository
-interface PortfolioStateRepository: JpaRepository<PortfolioState, Long> {
-}
