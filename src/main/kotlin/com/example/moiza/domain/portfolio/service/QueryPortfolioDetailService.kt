72b42ce5d9a815039185df90a9d857b016f0a939
김명진 <4mj.eo@dsm.hs.kr>
2025-02-02 21:21:17 +0900

    refactor(portfolio): service 클래스 이름 변경

--- src/main/kotlin/com/example/moiza/domain/portfolio/service/QueryPortfolioDetailService.kt
+++ src/main/kotlin/com/example/moiza/domain/portfolio/service/QueryPortfolioDetailService.kt
@@ -9,7 +9,7 @@ import org.springframework.data.repository.findByIdOrNull
 import org.springframework.stereotype.Service
 
 @Service
-class QueryPortfolioDetailResponse(
+class QueryPortfolioDetailService(
     private val portfolioRepository: PortfolioRepository,
 ) {
     @Transactional
