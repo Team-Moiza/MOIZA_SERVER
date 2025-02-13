3df839831fdcb0ab238d83200d0dfdd0b965abd2
김명진 <4mj.eo@dsm.hs.kr>
2025-02-14 01:13:05 +0900

    refactor(code): requestParam으로 등록

--- src/main/kotlin/com/example/moiza/domain/code/presentation/CodeController.kt
+++ src/main/kotlin/com/example/moiza/domain/code/presentation/CodeController.kt
@@ -4,6 +4,7 @@ import com.example.moiza.domain.code.presentation.dto.CodeResponse
 import com.example.moiza.domain.code.service.CreateCodeService
 import org.springframework.web.bind.annotation.PostMapping
 import org.springframework.web.bind.annotation.RequestMapping
+import org.springframework.web.bind.annotation.RequestParam
 import org.springframework.web.bind.annotation.RestController
 
 @RestController
@@ -12,6 +13,6 @@ class CodeController(
     private val createCodeService: CreateCodeService,
 ) {
     @PostMapping
-    fun createCode(keyword: String): CodeResponse
+    fun createCode(@RequestParam keyword: String): CodeResponse
         = createCodeService.execute(keyword)
 }
