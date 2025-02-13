8e8bc748ea9b3476b821d151cb318c2002cab000
김명진 <4mj.eo@dsm.hs.kr>
2025-02-13 21:01:16 +0900

    delete(code): 기존 코드 삭제

--- src/main/kotlin/com/example/moiza/domain/code/presentation/CodeController.kt
+++ src/main/kotlin/com/example/moiza/domain/code/presentation/CodeController.kt
@@ -1,26 +1,17 @@
 package com.example.moiza.domain.code.presentation
 
 import com.example.moiza.domain.code.presentation.dto.CreateCodeResponse
-import com.example.moiza.domain.code.presentation.dto.QueryCodeKeywordResponse
 import com.example.moiza.domain.code.service.CreateCodeService
-import com.example.moiza.domain.code.service.QueryCodeKeywordService
-import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.PostMapping
 import org.springframework.web.bind.annotation.RequestMapping
-import org.springframework.web.bind.annotation.RequestParam
 import org.springframework.web.bind.annotation.RestController
 
 @RestController
 @RequestMapping("/code")
 class CodeController(
     private val createCodeService: CreateCodeService,
-    private val queryCodeKeywordService: QueryCodeKeywordService,
 ) {
     @PostMapping
     fun createCode(keyword: String): CreateCodeResponse
         = createCodeService.execute(keyword)
-
-    @GetMapping
-    fun queryCodeKeyword(@RequestParam("code-id") codeId: Long): QueryCodeKeywordResponse
-        = queryCodeKeywordService.execute(codeId)
 }
--- src/main/kotlin/com/example/moiza/domain/code/presentation/dto/QueryCodeResponse.kt
+++ src/main/kotlin/com/example/moiza/domain/code/presentation/dto/QueryCodeResponse.kt
@@ -1,9 +1,5 @@
 package com.example.moiza.domain.code.presentation.dto
 
-data class QueryCodeKeywordResponse(
-    val keyword: String
-)
-
 data class CodeResponse(
     val id: Long,
     val keyword: String
