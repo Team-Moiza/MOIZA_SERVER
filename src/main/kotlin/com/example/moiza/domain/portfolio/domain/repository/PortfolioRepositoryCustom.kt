9ec13cef54e3861854b64203d0d173dc9a270a32
김명진 <4mj.eo@dsm.hs.kr>
2025-02-18 18:32:21 +0900

    feat(portfolio): 필터링 개발

--- src/main/kotlin/com/example/moiza/domain/portfolio/domain/repository/PortfolioRepositoryCustom.kt
+++ src/main/kotlin/com/example/moiza/domain/portfolio/domain/repository/PortfolioRepositoryCustom.kt
@@ -1,10 +1,11 @@
 package com.example.moiza.domain.portfolio.domain.repository
 
 import com.example.moiza.domain.portfolio.domain.type.UserStatus
+import com.example.moiza.domain.portfolio.presentation.dto.PortfolioFilter
 import com.example.moiza.domain.portfolio.presentation.dto.res.PortfolioListResponse
 import org.springframework.data.domain.Page
 import org.springframework.data.domain.Pageable
 
 interface PortfolioRepositoryCustom {
-    fun getPortfolioList(status: UserStatus, pageable: Pageable): Page<PortfolioListResponse>
+    fun getPortfolioList(status: UserStatus, pageable: Pageable, filter: PortfolioFilter): Page<PortfolioListResponse>
 }
