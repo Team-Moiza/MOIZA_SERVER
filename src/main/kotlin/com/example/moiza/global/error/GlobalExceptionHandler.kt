6b1d9b744970941e8f463e62eb5ef855b062c586
anys34 <anyeseong34@gmail.com>
2025-02-22 05:39:46 +0900

    feat(error): Method Exception 예외 핸들링 추가

--- src/main/kotlin/com/example/moiza/global/error/GlobalExceptionHandler.kt
+++ src/main/kotlin/com/example/moiza/global/error/GlobalExceptionHandler.kt
@@ -3,6 +3,7 @@ package com.example.moiza.global.error
 import com.example.moiza.global.error.exception.BusinessException
 import org.springframework.http.HttpStatus
 import org.springframework.http.ResponseEntity
+import org.springframework.web.bind.MethodArgumentNotValidException
 import org.springframework.web.bind.annotation.ExceptionHandler
 import org.springframework.web.bind.annotation.RestControllerAdvice
 
@@ -15,5 +16,17 @@ class GlobalExceptionHandler {
             ErrorResponse(code.status, code.message),
             HttpStatus.valueOf(code.status)
         )
+    }
+
+    @ExceptionHandler(MethodArgumentNotValidException::class)
+    fun handleBadRequestExceptions(e: MethodArgumentNotValidException): ResponseEntity<Map<String, Map<String, String>>> {
+        val errors = e.bindingResult.fieldErrors
+            .associateBy({ it.field }, { it.defaultMessage ?: "Invalid value" })
+
+        return ResponseEntity(getErrorsMap(errors), HttpStatus.BAD_REQUEST)
+    }
+
+    private fun getErrorsMap(errors: Map<String, String>): Map<String, Map<String, String>> {
+        return mapOf("errors" to errors)
     }
 }
