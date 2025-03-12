1b78e58f1071910f7cc2a2fdf21c0eea61f77d6a
anys34 <anyeseong34@gmail.com>
2025-03-12 22:20:14 +0900

    feat:: userstatus 반환

--- src/main/kotlin/com/example/moiza/domain/user/presentation/dto/res/InfoUserResponse.kt
+++ src/main/kotlin/com/example/moiza/domain/user/presentation/dto/res/InfoUserResponse.kt
@@ -1,5 +1,6 @@
 package com.example.moiza.domain.user.presentation.dto.res
 
+import com.example.moiza.domain.portfolio.domain.type.UserStatus
 import com.example.moiza.domain.user.domain.User
 import com.example.moiza.domain.user.domain.type.EducationStatus
 import com.example.moiza.domain.user.domain.type.Job
@@ -19,6 +20,7 @@ data class InfoUserResponse(
     val job: Job?,
     val company: String?,
     val introduce: String?,
+    val userStatus: UserStatus,
     ) {
     constructor(user: User): this(
         nickname = user.nickname,
@@ -32,5 +34,6 @@ data class InfoUserResponse(
         job = user.job,
         company = user.company,
         introduce = user.introduce,
+        userStatus = user.userStatus,
     )
 }
