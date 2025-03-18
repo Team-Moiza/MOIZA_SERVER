86d6255a128d4cca8e481a2b7a9bf2412f5340e3
anys34 <anyeseong34@gmail.com>
2025-03-18 20:15:18 +0900

    Feat:: response추가

--- src/main/kotlin/com/example/moiza/domain/like/presentation/dto/response/LikesResponse.kt
+++ src/main/kotlin/com/example/moiza/domain/like/presentation/dto/response/LikesResponse.kt
@@ -1,17 +1,24 @@
 package com.example.moiza.domain.like.presentation.dto.response
 
+import com.example.moiza.domain.code.presentation.dto.CodeResponse
 import com.example.moiza.domain.like.domain.Like
+import com.example.moiza.domain.user.domain.type.Job
 import com.example.moiza.domain.user.domain.type.Major
 import com.example.moiza.domain.user.domain.type.School
+import java.time.LocalDate
 
 data class LikesResponse(
     val portfolioId: Long,
     val name: String,
+    val company: String?,
     val profile: String,
+    val enrollmentStartDate: LocalDate?,
+    val job: Job?,
     val school: School?,
     val major: Major?,
     val introduce: String?,
     val likeCnt: Long,
+    val codes: List<CodeResponse>
 ) {
     companion object {
         fun from(like: Like): LikesResponse {
@@ -19,11 +26,20 @@ data class LikesResponse(
             return LikesResponse(
                 portfolioId = like.portfolio.id,
                 name = user.nickname,
+                company = user.company,
+                enrollmentStartDate = user.enrollmentStartDate,
+                job = user.job,
                 profile = user.profile,
                 school = user.school,
                 major = user.major,
                 introduce = user.introduce,
-                likeCnt = like.portfolio.likeCnt
+                likeCnt = like.portfolio.likeCnt,
+                codes = like.portfolio.codes.map { portfolioCode ->
+                    CodeResponse(
+                        id = portfolioCode.code.id,
+                        keyword = portfolioCode.code.keyword
+                    )
+                },
             )
         }
     }
