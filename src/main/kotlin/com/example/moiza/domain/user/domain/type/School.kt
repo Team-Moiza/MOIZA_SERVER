4ad95eb8364673fe214ee6dbcfe152603896ec5b
anys34 <anyeseong34@gmail.com>
2025-03-24 20:47:00 +0900

    feat(portfolio): 한글 명칭 추가

--- src/main/kotlin/com/example/moiza/domain/user/domain/type/School.kt
+++ src/main/kotlin/com/example/moiza/domain/user/domain/type/School.kt
@@ -1,8 +1,12 @@
 package com.example.moiza.domain.user.domain.type
 
-enum class School {
-    BSSM,
-    DGSM,
-    DSM,
-    GSM,
+enum class School(val schoolName: String) {
+    BSSM("부산소프트웨어마이스터고등학교"),
+    DGSM("대구소프트웨어마이스터고등학교"),
+    DSM("대덕소프트웨어마이스터고등학교"),
+    GSM("광주소프트웨어마이스터고등학교");
+
+    fun getName(): String {
+        return schoolName
+    }
 }
